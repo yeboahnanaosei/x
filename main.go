15ae5350afb5c79287b4c8e84e71fbf4f0ba708f
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2021-04-15 01:32:13 +0000

    fix port

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", index)
-	log.Fatal(http.ListenAndServe(":9900", nil))
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
